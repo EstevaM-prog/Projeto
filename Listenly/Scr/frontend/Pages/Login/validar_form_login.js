73f4075c387c10387aa2d742cf85802ad2e45527
EstevaM-prog <estevamx9@gmail.com>
2025-09-02 00:42:56 -0300

    att

--- Listenly/Scr/frontend/Pages/Login/validar_form_login.js
+++ Listenly/Scr/frontend/Pages/Login/validar_form_login.js
@@ -5,19 +5,34 @@ function validarFormLogin(event) {
 
     const email = document.getElementById('email').value.trim();
     const senha = document.getElementById('senha').value.trim();
+    const mensagem = document.getElementById('mensagem');
 
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
     const regexSenha = /^(?=.*[a-zA-Z])(?=.*[\d\W_]).{10,}$/; 
 
-    if (email === "" || senha === "") {
-        alert("❌ Por favor, preencha todos os campos.");
-    } else if (!regexEmail.test(email)) {
-        alert("❌ Insira um e-mail válido.");
-    } else if (!regexSenha.test(senha)) {
-        alert("❌ A senha deve ter pelo menos 10 caracteres, incluindo letras e números ou caractere especial.");
-    } else {
-        alert("✅ Login válido! (simulação)");
-        // aqui você poderia enviar o formulário com form.submit() se quiser
-    }
-} 
 
+    function exibirErro(texto){
+        alert(texto);
+        mensagem.textContent = texto;
+        mensagem.className = "erro";
+    }
+
+    if (email === "" || senha === "") {
+        exibirErro("❌ Por favor, preencha todos os campos.");
+        return false;
+    }
+    if (!regexEmail.test(email)){
+        exibirErro("❌ Insira um e-mail válido.");
+    }
+    if (!regexSenha.test(senha)){
+        exibirErro("❌ A senha deve ter pelo menos 10 caracteres, incluindo letras e números ou caractere especial.");
+    }
+
+    //Sucesso
+    const sucessoTexto = "✅  Login válido";
+    alert(sucessoTexto);
+    mensagem.textContent = sucessoTexto;
+    mensagem.className = "sucesso";
+
+    return true;
+}
